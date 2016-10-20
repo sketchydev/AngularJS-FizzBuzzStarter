446b1c4a0837083735b4a5dbe6ffa464a504dc41
Shane Booth <shane.booth@advancedcomputersoftware.com>
2016-10-20 10:58:03 +0100

    remove all the seed stuff we don't need

---
+++ app/FizzBuzz/fizz-buzz.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp.fizzBuzz')
+.controller('FizzBuzz', [function() {
+
+}]);
--- app/FizzBuzz/fizz_buzz_test.js
+++ app/FizzBuzz/fizz_buzz_test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.FizzBuzz module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.FizzBuzz'));
 
-  describe('view1 controller', function(){
+  describe('FizzBuzz controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body

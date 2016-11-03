a813e536a19c263c3c35b2ee35a72b92d7aa82bb
Kamalpreet Singh Chauhan <kmlprtsng@users.noreply.github.com>
2016-11-03 13:16:19 +0000

    Update fizz-buzz.js

--- app/FizzBuzz/fizz-buzz.js
+++ app/FizzBuzz/fizz-buzz.js
@@ -4,14 +4,34 @@
     angular.module('myApp.fizzBuzz',[])
         .controller('FizzBuzzController', fizzBuzzController);
 
-    fizzBuzzController.$inject = ["$scope"];
+    fizzBuzzController.$inject = ['$scope'];
 
     function fizzBuzzController($scope){
         var vm = this;
 
+        vm.playFizzBuzz = function(){
+            var fizzBuzzArray = [];
+
+            for(var i=1; i<=100; i++){
+                fizzBuzzArray.push(vm.convert(i));
+            }
+
+            console.log(fizzBuzzArray);
+
+            return fizzBuzzArray;
+        };
+
         vm.convert = function(input)
         {
+            var returnVal = '';
 
+            if(input%3 === 0)
+                returnVal = 'Fizz';
+
+            if(input%5 === 0)
+                returnVal = returnVal + 'Buzz';
+
+            return returnVal || input;
         };
     }
 })();
